84e92542fb193010b958df426121b1614cd91b6f
ec18213 <s.akhtar@se18.qmul.ac.uk>
2022-05-23 20:18:54 +0100

    Basic blog style app to refresh on react knowledge and delve more deeper into funtional components rather than class bases

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+// newer version don't need a react import
+import Navbar from './Navbar'
+import Home from './Home'
+import Create from './Create'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
+// ignore this is for redux toolkit
+// import { decrement, increment, incrementByAmount } from './redux/slices/counterSlice';
+// import { useSelector, useDispatch } from 'react-redux';
 
 function App() {
+  // ignore this is for redux toolkit
+  // const count = useSelector(state => state.counter.value)
+  // const dispatch = useDispatch()
+
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Nest Navbar */}
+      <Navbar />
+
+      <div className="content">
+        <Switch>
+
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+          <Route path="/create">
+            <Create />
+          </Route>
+
+          {/* Dynamic Path */}
+          <Route path="/blogs/:id">
+            <BlogDetails />
+          </Route>
+
+          {/* picks up any path, commonly used to serve a 404 page to user */}
+          <Route path="*">
+            <NotFound />
+          </Route>
+
+        </Switch>
+      </div>
+
     </div>
+    </Router>
   );
 }
 
+// export default allows components to be used in other files
 export default App;
